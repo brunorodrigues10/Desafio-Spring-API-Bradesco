e5c9297f29e093a619e09951b29a0ddb58e44cce
Bruno <brunoiannirodrigues1@gmail.com>
2025-03-14 19:09:27 -0300

    First Commit
    Criação do repositório do projeto.

---
+++ desafio-spring-api-bradesco/src/test/java/me/dio/desafio_spring_api_bradesco/DesafioSpringApiBradescoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.desafio_spring_api_bradesco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringApiBradescoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
